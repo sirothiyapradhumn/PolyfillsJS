90d2ed72a48918314fe631b6dfc01174f7a9478e
sirothiyapradhumn <sirothiyapradhumn@gmail.com>
2024-09-25 11:41:53 +0530

    custom memoized fn

--- JsProblems/memoization.js
+++ JsProblems/memoization.js
@@ -27,3 +27,22 @@ return () => {
 
 console.log(memoizeClouser(8)());
 console.log(memoizeClouser(8)());
+
+
+const memoized = (cbFn) => {
+    const cache = {};
+    return (...args) => {
+        const key = args.sort((a,b) => a-b).join("_")
+        if(key in cache) {
+            return console.log('return from cache', cache[key]);
+        }
+        cache[key] = cbFn(args[0], args[1]);
+        return console.log('resister on cache', cache[key]);
+    }
+}
+
+const sum = (a,b) => a+b;
+const test = memoized(sum);
+const t1 = test(10,20);
+const t2 = test(10,20);
+const t3 = test(20,10);
